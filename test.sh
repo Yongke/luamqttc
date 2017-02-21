6e6762e4c5e896ac369e625b1b1563393da8d0ab
Chris Liu <liugh@rippletek.com>
2017-02-21 17:59:00 +0800

    make sure python3 is available

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ luarocks make
 
 git clone https://github.com/eclipse/paho.mqtt.testing.git
 oldpwd=`pwd`
-cd paho.mqtt.testing/interoperability && python3 startbroker.py &
+cd paho.mqtt.testing/interoperability && python3 startbroker.py
 sleep 3
 cd $oldpwd && lua tests/client_test.lua
