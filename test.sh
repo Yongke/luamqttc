6241a7c1a5440b7386b6bcc11b040baed11ff44b
Chris Liu <liugh@rippletek.com>
2017-02-21 18:02:31 +0800

    wait longer for broker

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ luarocks make
 
 git clone https://github.com/eclipse/paho.mqtt.testing.git
 oldpwd=`pwd`
-cd paho.mqtt.testing/interoperability && python3 startbroker.py
-sleep 3
+cd paho.mqtt.testing/interoperability && python3 startbroker.py &
+sleep 10
 cd $oldpwd && lua tests/client_test.lua
